7cd76161ce34d91a8829184c7fb2e0b8518dd505
Vladimir Proshin <VlaShin1986@gmail.com>
2023-04-23 22:08:24 +0300

    added button with onClick event

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,13 +1,19 @@
-const users = document.getElementById("users-table");
-const path = window.location.pathname;
-
-const chatId = path.split("/")[1];
-document.addEventListener("DOMContentLoaded", () => {
-    fetchUsers().then((data) => renderUsersTable(data)).catch((error) => console.log(error))
-})
+const usersTable = document.getElementById("users-table-content");
+const button = document.getElementById("fetch-users-button");
+// const path = window.location.pathname;
+//
+// const chatId = path.split("/")[1];
+function myFunction(){
+    button.addEventListener("click",() => {
+        fetchUsers().then((data) => renderUsersTable(data)).catch((error) => console.log(error))
+    })
+}
+// button.addEventListener("click",() => {
+//     fetchUsers().then((data) => renderUsersTable(data)).catch((error) => console.log(error))
+// })
 
 function fetchUsers() {
-    return fetch("/admin/" + chatId + "/users").then((response) => {
+    return fetch("http://localhost:8080/admin/users/545081236").then((response) => {
         if (!response.ok) {
             throw new Error(response.statusText);
         }
@@ -22,7 +28,7 @@ function renderUsersTable(users) {
             <td>${user.chatId}</td>
             <td>${user.admin}</td>
             <td>${user.name}</td>
-            <td>${user.phone}</td> `
+            <td>${user.phone}</td> </tr>`
     }).join("");
-    users.innerHTML = markup;
+    usersTable.innerHTML = markup;
 }
